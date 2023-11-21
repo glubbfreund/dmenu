1ca025754f5c8c28b0a325128084e5e3ff36243c
Glubbfreund <oliver.leis@gmail.com>
2023-11-21 21:19:55 +0100

    changed colors

--- config.h
+++ config.h
@@ -10,10 +10,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#1c1c1c" },
+	[SchemeNorm] = { col_strong, "#1c1c1c" },
 	[SchemeSel] = { "#bbbbbb", col_highlight },
 	[SchemeSelHighlight] = { "#eeeeee", col_highlight },
-	[SchemeNormHighlight] = { col_strong, "#1c1c1c" },
+	[SchemeNormHighlight] = { "#bbbbbb", "#1c1c1c" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
