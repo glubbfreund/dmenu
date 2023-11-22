0eaf461253e4e4ef21b7851ad798469a1fe20a05
Glubbfreund <oliver.leis@gmail.com>
2023-11-22 08:04:18 +0100

    regenerated config.def.h

--- config.def.h
+++ config.def.h
@@ -1,19 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+#include "../colors/colors.h"
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font Mono:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { col_strong, "#1c1c1c" },
+	[SchemeSel] = { "#bbbbbb", col_highlight },
+	[SchemeSelHighlight] = { "#eeeeee", col_highlight },
+	[SchemeNormHighlight] = { "#bbbbbb", "#1c1c1c" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeInput] = { "#bbbbbb", "#1c1c1c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
