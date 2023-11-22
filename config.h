dd3e8b40f5967ebfcfd8b8e8cd2e9e7c40b31470
Glubbfreund <oliver.leis@gmail.com>
2023-11-22 08:11:34 +0100

    just styling improvements

--- config.h
+++ config.h
@@ -3,25 +3,25 @@
 #include "../colors/colors.h"
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"JetBrainsMono Nerd Font Mono:size=13"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { col_strong, "#1c1c1c" },
-	[SchemeSel] = { "#bbbbbb", col_highlight },
-	[SchemeSelHighlight] = { "#eeeeee", col_highlight },
-	[SchemeNormHighlight] = { "#bbbbbb", "#1c1c1c" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeInput] = { "#bbbbbb", "#1c1c1c" },
-};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrainsMono Nerd Font Mono:size=13"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] =          { col_strong, "#1c1c1c"     },
+	[SchemeSel] =           { "#bbbbbb", col_highlight  },
+	[SchemeSelHighlight] =  { "#eeeeee", col_highlight  },
+	[SchemeNormHighlight] = { "#bbbbbb", "#1c1c1c"      },
+	[SchemeOut] =           { "#000000", "#00ffff"      },
+	[SchemeInput] =         { "#bbbbbb", "#1c1c1c"      },
+};
