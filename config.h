3c9094e7d31dbeea3f89ba6599a09ed11fb0a5f1
Glubbfreund <oliver.leis@gmail.com>
2023-11-24 16:08:06 +0100

    changed width

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 static int topbar           = 1;    /* -b  option; if 0, dmenu appears at bottom     */
 static int dmx              = 160;  /* put dmenu at this x offset */
 static int dmy              = 0;    /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
-static unsigned int dmw     = 1500; /* make dmenu this wide */
+static unsigned int dmw     = 1633; /* make dmenu this wide */
 static unsigned int lines   = 0;    /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static const char *prompt   = NULL; /* -p  option; prompt to the left of input field */
 
