8cae2be4727e717c97951964d7b218c76def6c65
Glubbfreund <oliver.leis@gmail.com>
2023-11-24 15:13:32 +0100

    moved xyz patch settings to config

--- config.h
+++ config.h
@@ -2,19 +2,22 @@
 /* Default settings; can be overriden by command line. */
 #include "../colors/colors.h"
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static int topbar           = 1;    /* -b  option; if 0, dmenu appears at bottom     */
+static int dmx              = 200;  /* put dmenu at this x offset */
+static int dmy              = 0;    /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
+static unsigned int dmw     = 1500; /* make dmenu this wide */
+static unsigned int lines   = 0;    /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static const char *prompt   = NULL; /* -p  option; prompt to the left of input field */
+
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-/* -fn option overrides fonts[0]; default X11 font or font set */
+
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font Mono:size=12"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
